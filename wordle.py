8a0dcf3def85231b6d796f314dedc1714ca15a08
unknown <renancavalcantercb@protonmail.com>
2023-01-15 02:20:55 -0300

    refactor to work with classes

--- wordle.py
+++ wordle.py
@@ -1,75 +1,75 @@
-import json
-import random
-from urllib.request import urlopen
-
-import unidecode
+from utils import utils
 from colorama import Fore
 
 
-def get_word_list():
-    url = "https://gist.githubusercontent.com/un-versed/6373912fbf4649704b6823ea696cfcb1/raw" \
-          "/629137a0d0c7160b94c35013df8d570b31100174/termooo-wordsv2.json "
-    response = urlopen(url)
-    data = json.loads(response.read())
-    return data
+class Player:
+    def __init__(self):
+        pass
+
+    def get_guess(self):
+        return input('Enter a word: ').lower()
+
+    def display_result(self, result):
+        print(' '.join(result))
+
+    def display_attempts(self, attempts):
+        print(f'You have {str(5 - attempts)} guesses left.')
+
+    def display_win(self):
+        print('Congratulations, you won!')
+
+    def display_lose(self, word):
+        print(f'You lose!\nThe correct word was: {word.lower()}')
+
+    def display_invalid_input(self):
+        print('Invalid input, please try again')
+
+    def display_invalid_word(self, word):
+        print(f'{word.lower()} is not a valid word')
 
 
-def remove_accent_from_word():
-    data = get_word_list()
-    for i in range(len(data)):
-        data[i] = unidecode.unidecode(data[i])
-    return data
+class Game:
+    def __init__(self):
+        self.attempts = 0
+        self.words = utils.remove_accent_from_word(utils.get_word_list())
+        self.random_word = utils.get_random_word(self.words)
+        self.result = []
+        self.end_of_game = False
+        self.player = Player()
 
+    def check_word_in_word_list(self, guess):
+        return guess in self.words
 
-def get_random_word():
-    data = remove_accent_from_word()
-    return random.choice(data)
-
-
-def check_word_in_word_list(guess):
-    data = remove_accent_from_word()
-    if guess in data:
-        return True
-    else:
-        return False
-
-
-def main():
-    end_of_game = False
-    attempts = 0
-    random_word = get_random_word()
-    player_guess = input('Enter a word: ').lower()
-    result = []
-    while not end_of_game:
-        if not isinstance(player_guess, str) or len(player_guess) != len(random_word):
-            print('Invalid input, please try again')
-            player_guess = input('Enter a word: ').lower()
-        elif player_guess == random_word:
-            print('Congratulations, you won!')
-            end_of_game = True
-        elif not check_word_in_word_list(player_guess):
-            print(f'{player_guess.lower()} is not a valid word')
-            player_guess = input('Enter a word: ').lower()
-        else:
-            attempts += 1
-            if attempts == 5:
-                print(f'You lose!\nThe correct word was: {random_word.lower()}')
-                end_of_game = True
+    def play(self):
+        while not self.end_of_game:
+            player_guess = self.player.get_guess()
+            if not isinstance(player_guess, str) or len(player_guess) != len(self.random_word):
+                self.player.display_invalid_input()
+            elif player_guess == self.random_word:
+                self.player.display_win()
+                self.end_of_game = True
+            elif not self.check_word_in_word_list(player_guess):
+                self.player.display_invalid_word(player_guess)
             else:
-                for i in range(len(random_word)):
-                    if player_guess[i] == random_word[i]:
-                        result.append(
-                            Fore.GREEN + player_guess[i] + Fore.RESET)
-                    elif player_guess[i] in random_word:
-                        result.append(
-                            Fore.YELLOW + player_guess[i] + Fore.RESET)
-                    else:
-                        result.append(
-                            Fore.RED + '_' + Fore.RESET)
-                print(' '.join(result))
-                result = []
-                print(f'You have {str(5 - attempts)} guesses left.')
-                player_guess = input('Enter a word: ').lower()
+                self.attempts += 1
+                if self.attempts == 5:
+                    self.player.display_lose(self.random_word)
+                    self.end_of_game = True
+                else:
+                    for i in range(len(self.random_word)):
+                        if player_guess[i] == self.random_word[i]:
+                            self.result.append(
+                                Fore.GREEN + player_guess[i] + Fore.RESET)
+                        elif player_guess[i] in self.random_word:
+                            self.result.append(
+                                Fore.YELLOW + player_guess[i] + Fore.RESET)
+                        else:
+                            self.result.append(
+                                Fore.RED + '_' + Fore.RESET)
+                    self.player.display_result(self.result)
+                    self.result = []
+                    self.player.display_attempts(self.attempts)
 
 
-main()
+game = Game()
+game.play()
